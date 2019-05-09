d7e740d835a91499444b7232443da32f2e1d7039
Dayron <dayronyaret@gmail.com>
2019-05-09 16:09:48 +0100

    Initial commit

---
+++ app/src/main/java/es/ulpgc/dayron/repaso/MainActivity.java
@@ -0,0 +1,13 @@
+package es.ulpgc.dayron.repaso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
